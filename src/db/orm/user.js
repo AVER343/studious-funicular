9195821698724164a4ceab532f461479ab1b4e4b
Ankush verma <low_profile@Ankushs-MacBook-Air.local>
2021-05-15 01:55:56 +0530

    Reset password , email verification added (using OTP)

--- src/db/orm/user.js
+++ src/db/orm/user.js
@@ -1,7 +1,9 @@
 const pool = require('../postgres')
 let bcrypt = require('bcryptjs')
 let jwt = require('jsonwebtoken')
-const emailVerificationTemplate = require('../../utils/templates/email-verification')
+const sgMail = require('@sendgrid/mail');
+const emailVerificationTemplate = require('../../utils/templates/email-verification');
+const { query } = require('../postgres');
 class User{
     constructor(params){
         let keys = Object.keys(params)
@@ -12,7 +14,6 @@ class User{
     }
 
     async createJWT(){
-        console.log(this)
         let json_web_token = await jwt.sign({...this,jwt_created_time:Date.now()},process.env.JWT_SECRET)
         this.jwt = json_web_token
     }
@@ -33,10 +34,30 @@ class User{
                 this.deleteFields()
                 this.createJWT()
     }
-    async sendEmailVerificationMail(){
+    static async generateOTP(email){
+        let OTP = Math.ceil(Math.random() * 20000 + Math.random()*10000)
+        if(OTP<1000)
+        {
+            OTP = OTP + 999 + Math.random()*10000
+        }
+        let user = await pool.query('SELECT * FROM USERS WHERE EMAIL = $1',[email])
+        await pool.query('INSERT INTO USER_OTP(user_id,otp) VALUES($1,$2)',[user.rows[0].id,OTP])
+        return OTP
+    }
+    async sendEmailVerificationMail({reset_password = false}){
         const user = this;
         let message = '<h2><b>Verification code :<b></h2>'
-        emailVerificationTemplate({name:user.user_name,message})
+        let OTP = await User.generateOTP(user.email)
+        let from=process.env.FROM
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+              to: user.email,
+              from,
+              subject:'Signup Confirmation !',
+              text:'Signup Confirmation !',
+              html: emailVerificationTemplate({name:user.user_name,message,OTP,reset_password})
+          }
+        await sgMail.send(msg)
     }
     static async findOne({email}){
         const user = await pool.query('SELECT * FROM USERS WHERE email = $1',[email]);
@@ -44,7 +65,9 @@ class User{
         {
             throw new Error('No user with the provided email exists !')
         }
-        return user.rows[0]
+        let new_user = new User({...user.rows[0]})
+        new_user.deleteFields()
+        return new_user
     }
     static async verifyLogin(email,password){
         let user = await pool.query('SELECT * FROM USERS WHERE email = $1',[email])
@@ -52,6 +75,10 @@ class User{
         if(!isRightPassword)
         {
            throw new Error('Invalid credentials !')
+        }
+        if(!user.rows[0]['email_verified'])
+        {
+            throw new Error('Please verify your email !')
         }
         let new_user = new User({...user.rows[0]})
         new_user.deleteFields()
@@ -61,6 +88,47 @@ class User{
         delete this.password
         delete this.modified_time
         delete this.user_active
+    }
+    static async verifyOTP({email,otp}){
+        try{
+            const user = await User.findOne({email})
+            const qu = await pool.query('select * from user_otp');
+            console.log({qu:qu.rows})
+            const  database_otp = await pool.query(`SELECT * FROM USER_OTP
+                                                     WHERE user_id= $1 and otp = $2  
+                                                     and id = (SELECT MAX(id) FROM USER_OTP WHERE user_id= $1)
+                                                     and generated_on > NOW() - INTERVAL '15 minutes '
+                                                     and otp_active = true;`,[user.id,otp])
+            if(database_otp.rowCount==0){
+           try{
+                    await pool.query(`UPDATE USER_OTP 
+                    SET OTP_TRIED_FOR = OTP_TRIED_FOR + 1
+                WHERE user_id = $1 and id = (SELECT MAX(id) FROM USER_OTP 
+                                                WHERE user_id= $1)`,[user.id])
+           }
+           catch(e){
+            throw new Error('Wrong OTP provided or OTP expired !');
+           }
+           throw new Error('Wrong OTP provided or OTP expired !');
+        }
+            await pool.query(`UPDATE USER_OTP 
+                                SET OTP_ACTIVE = FALSE
+                            WHERE id=$1`,[database_otp.rows[0].id])
+           await pool.query(`UPDATE USERS 
+                                SET email_verified =true , user_active = true
+                               WHERE email = $1 and id = $2::integer returning *`,[email,user.id])
+            user['email_verified']= true
+            user.deleteFields()
+            user.createJWT()
+            return user
+        }
+        catch(e){
+            throw new Error(e.message)
+        }
+    }
+    async changePassword(new_password){
+        this.password = bcrypt.hashSync(new_password,parseInt(process.env.HASHING_ITERATIONS))
+        await pool.query('UPDATE USERS SET PASSWORD =$1 WHERE id=$2',[this.password,this.id])
     }
 }
 module.exports = User
--- src/routes/user.js
+++ src/routes/user.js
@@ -7,7 +7,8 @@ let defaultError = require('../utils/error-handling/error')
 router.post('/register',
         body('email').isEmail(),
         body('password').isLength({min:5}),
-        body('user_name').isLength({min:1})
+        body('user_name').isLength({min:1}),
+        body('confirm_password').isLength({min:5})
         ,async (req,res)=>{
             try{
                 const errors = validationResult(req);
@@ -21,14 +22,23 @@ router.post('/register',
                 }
                 let user = new User({user_name,email,password})
                 await user.save()
+                await user.sendEmailVerificationMail({reset_password:false})
+                delete user.jwt
                res.status(201).send({user})
             }
             catch(e){
                 defaultError({res,e})
             }
 })
-router.post('/login',async(req,res)=>{
+router.post('/login',
+        body('email').isEmail(),
+        body('password').isLength({min:5}),  
+async(req,res)=>{
    try{
+            const errors = validationResult(req);
+            if (!errors.isEmpty()){
+                return defaultError({e:errors.array(),res})
+            }
         let {email,password} = req.body
         await User.findOne({email})
         let user = await User.verifyLogin(email,password)
@@ -39,4 +49,45 @@ router.post('/login',async(req,res)=>{
        defaultError({res,e})
    }
 })
+router.post('/email/otp',
+            body('email').isEmail(),
+            body('otp').isLength({min:4}),
+        async(req,res)=>{
+    try{ 
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return defaultError({e:errors.array(),res})
+        }
+         let {email,otp,password} = req.body
+         ///check if user exists
+         let user = await User.verifyOTP({email,otp})
+         if(password){
+             await user.changePassword(password)
+             return res.send({messages:[{message:'Password has been changed !'}]})
+         }
+         res.send(user)
+    }
+    catch(e){
+        defaultError({res,e})
+    }
+ })
+ router.post('/reset/password',
+            body('email').isEmail(),
+        async(req,res)=>{
+    try{ 
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return defaultError({e:errors.array(),res})
+        }
+         let {email} = req.body
+         ///check if user exists
+         let user = await User.findOne({email})
+        await user.sendEmailVerificationMail({reset_password:true})
+        res.send({messages:[{message:'OTP sent to your email !'}]})
+    }
+    catch(e){
+        defaultError({res,e})
+    }
+ })
+
 module.exports = router
