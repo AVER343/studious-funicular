ea9c62ca7c91e440d857686ee6de0b72f25d5cec
AVER343 <ankushvermagb@gmail.com>
2021-05-25 20:40:19 +0530

    asdasd

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -4,7 +4,7 @@ import { store ,persistor} from '../src/store'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import '../styles/index.css'
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }){
   return  <Provider store={store}>
             <PersistGate persistor={persistor}>
               <ChakraProvider>
--- client/pages/login/index.jsx
+++ client/pages/login/index.jsx
@@ -1,43 +1,37 @@
-import {Formik,Form,Field} from 'formik'
-import {FormErrorMessage,Input,Button,FormControl,FormLabel} from '@chakra-ui/react'
-function LoginPage() {
-    function validateName(value) {
-      let error
-      if (!value) {
-        error = "Name is required"
-      } else if (value.toLowerCase() !== "naruto") {
-        error = "Jeez! You're not a fan 😱"
-      }
-      return error
-    }
-  
-    return (
-      <Formik
-        initialValues={{ name: "" }}
-        onSubmit={(values, actions) => actions.setSubmitting(true)}
-      >
-        {(props) => (
-          <Form>
-            <Field name="name" validate={validateName}>
-              {({ field, form }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel htmlFor="name">First name</FormLabel>
-                  <Input {...field} id="name" placeholder="name" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={props.isSubmitting}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    )
-  }
-  export default LoginPage
+import React,{useEffect} from 'react'
+import Image from 'next/image'
+import SignInSignUp from './both_form'
+import {
+  ThemeProvider,
+  theme,
+  ColorModeProvider,
+  CSSReset,
+  Box,
+  Flex,
+  IconButton,
+  useColorMode,
+  Heading,
+  Text,
+  Link,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Checkbox,
+  Button,
+  Grid
+} from '@chakra-ui/react'
+import {TouchableOpacity} from 'react-native-web'
+const VARIANT_COLOR = 'teal'
+import styles from './styles.module.css'
+const App = () => {
+  useEffect(()=>{
+
+  },[])
+  return (
+        <SignInSignUp/>
+  )
+}
+
+
+export default App
